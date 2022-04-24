3e5e1039ad43458544beddfd186620e9a1a24c5b
DrgnFireYellow <86269995+DrgnFireYellow@users.noreply.github.com>
2022-04-24 15:28:27 -0400

    begin to add bar charts

--- rich/charts.py
+++ rich/charts.py
@@ -1,1 +1,7 @@
-
+class Bar:
+    def __init__(self, max_width):
+        self.renderable = """"""
+        self.width = max_width
+        return self.renderable
+    def add_sequence(data, label):
+        self.renderable = self.renderable.join("─"*data)
